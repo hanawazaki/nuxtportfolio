71334832bbe46deee410afd8b4cd4abce9fccfda
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2023-07-06 15:47:22 +0700

    update fix

--- app.vue
+++ app.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex flex-col min-h-screen">
-    <Navbar />
+    <SiteHeaderVue />
     <main
       class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow items-start w-full"
     >
       <NuxtPage />
     </main>
-    <Footer />
+    <SiteFooter />
   </div>
 </template>
 
--- plugins/apollo.js
+++ plugins/apollo.js
@@ -1,8 +1,8 @@
 export default defineNuxtPlugin((nuxtApp) => {
   const { githubToken } = useRuntimeConfig();
-
   nuxtApp.hook("apollo:auth", ({ client, token }) => {
-    token.value = githubToken
+    token.value = githubToken;
   });
-})
+});
+
 
